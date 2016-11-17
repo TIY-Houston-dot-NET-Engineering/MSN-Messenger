b025e7b9e2485fe2aaf4fd57de8706e3bdd7b763
ShadiJam <shadijam00@gmail.com>
2016-11-17 14:08:32 -0600

    models and API add

--- Controllers/API.cs
+++ Controllers/API.cs
@@ -2,25 +2,31 @@ using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
-[Route("/api/card")]
-public class CardController : CRUDController<Card> {
-    public CardController(IRepository<Card> r) : base(r){}
+[Route("/api/handle")]
+public class HandleController : CRUDController<Handle> {
+    public HandleController(IRepository<Handle> r) : base(r){}
 
     [HttpGet("search")]
     public IActionResult Search([FromQuery]string term, int listId = -1){
-        return Ok(r.Read(dbset => dbset.Where(card => 
-            card.Title.ToLower().IndexOf(term.ToLower()) != -1
-            || card.Text.ToLower().IndexOf(term.ToLower()) != -1
+        return Ok(r.Read(dbset => dbset.Where(handle => 
+            handle.Name.ToLower().IndexOf(term.ToLower()) != -1
         )));
     }
 }
 
-[Route("/api/cardlist")]
-public class CardListController : CRUDController<CardList> {
-    public CardListController(IRepository<CardList> r) : base(r){}
+[Route("/api/message")]
+public class MessageController : CRUDController<Message> {
+    public MessageController(IRepository<Message> r) : base(r){}
 }
 
-[Route("/api/board")]
-public class BoardController : CRUDController<Board> {
-    public BoardController(IRepository<Board> r) : base(r){}
+[Route("/api/chatroom")]
+public class ChatroomController : CRUDController<Chatroom> {
+    public ChatroomController(IRepository<Chatroom> r) : base(r){}
+    
+    [HttpGet("search")]
+    public IActionResult Search([FromQuery]string term, int listId = -1){
+        return Ok(r.Read(dbset => dbset.Where(chatroom => 
+            chatroom.Name.ToLower().IndexOf(term.ToLower()) != -1
+        )));
+    }
 }
--- Models/Models.cs
+++ Models/Models.cs
@@ -11,53 +11,57 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-public class Card : HasId
+public class Handle : HasId
+{
+    [Required] 
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public List<Chatroom> Chatrooms { get; set; } = new List<Chatroom>();
+   
+}
+
+public class Chatroom : HasId 
 {
     [Required]
     public int Id { get; set; }
     [Required]
-    public string Title { get; set; }
+    public string Name { get; set; } 
+    public List<Message> Messages { get; set; } = new List<Message>();
+    
+}
+
+public class Message : HasId 
+{
     [Required]
-    [StringLength(250, MinimumLength = 10)]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(250)]
     public string Text { get; set; }
-
-    public int CardListId {get;set;}
+    public Handle handle { get; set; }
+    public int HandleId { get; set; }
+    public Chatroom chatroom { get; set; }
+    public int ChatroomId { get; set; }
+    [Required]
+    public DateTime createdAt { get; set; }
+    
 }
-
-public class CardList : HasId {
-    [Required]
-    public int Id { get; set; }
-    [Required]
-    public string Summary { get; set; }
-    [Required]
-    public List<Card> Cards { get; set; }
-
-    public int BoardId {get;set;}
-}
-
-public class Board : HasId {
-    [Required]
-    public int Id { get; set; }
-    [Required]
-    public string Title { get; set; }
-    [Required]
-    public List<CardList> Lists { get; set; }
-}
+    
 
 // declare the DbSet<T>'s of our DB context, thus creating the tables
 public partial class DB : IdentityDbContext<IdentityUser> {
-    public DbSet<Card> Cards { get; set; }
-    public DbSet<CardList> CardLists { get; set; }
-    public DbSet<Board> Boards { get; set; }
+    public DbSet<Handle> Handles { get; set; }
+    public DbSet<Chatroom> Chatrooms { get; set; }
+    public DbSet<Message> Messages { get; set; }
 }
 
 // create a Repo<T> services
 public partial class Handler {
     public void RegisterRepos(IServiceCollection services){
-        Repo<Card>.Register(services, "Cards");
-        Repo<CardList>.Register(services, "CardLists",
-            d => d.Include(l => l.Cards));
-        Repo<Board>.Register(services, "Boards",
-            d => d.Include(b => b.Lists).ThenInclude(l => l.Cards));
+        Repo<Handle>.Register(services, "Handles");
+        Repo<Message>.Register(services, "Messages",
+            d => d.Include(h => h.handle));
+        Repo<Chatroom>.Register(services, "Chatrooms",
+            d => d.Include(m => m.Messages).ThenInclude(h => h.handle));
     }
 }
